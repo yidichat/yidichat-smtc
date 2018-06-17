2371471d813be64665a4d7cbcacd47ff002cc9f2
yidichat <yidichat@gmail.com>
2018-06-17 22:25:22 +0300

    urlencode and curl

--- bot.php
+++ bot.php
@@ -3,7 +3,19 @@ $upd = json_decode(file_get_contents('php://input'), true);
 $uid = $upd['message']['from']['id'];
 file_get_contents("https://api.telegram.org/bot263336681:AAF9QR9Y17jBSk5mlvcG9Vmko4o_5wY4YJw/sendChatAction?chat_id=$uid&action=typing");
 $msg = urlencode($upd['message']['text']);
-$name = $upd['message']['from']['first_name'];
+$name = urlencode($upd['message']['from']['first_name']);
 $res = file_get_contents("http://api.brainshop.ai/get?bid=219&key=5laVJtJPqcXzHMRe&uid=$uid-$name&msg=$msg");
-$rep = json_decode($res, true)['cnt'];
+$rep = urlencode(json_decode($res, true)['cnt']);
 file_get_contents("https://api.telegram.org/bot263336681:AAF9QR9Y17jBSk5mlvcG9Vmko4o_5wY4YJw/sendMessage?chat_id=$uid&parse_mode=HTML&text=$rep");
+function curl_get_contents($url){
+  static $ch;
+  if !$ch $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+  return curl_exec($ch)
+}
