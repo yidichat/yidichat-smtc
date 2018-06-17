2301c7abba0265ac137c96a5c03d1707e86f8cb6
yidichat <yidichat@gmail.com>
2018-06-17 22:28:29 +0300

    fix curl

--- bot.php
+++ bot.php
@@ -1,12 +1,12 @@
 <?php
 $upd = json_decode(file_get_contents('php://input'), true);
 $uid = $upd['message']['from']['id'];
-file_get_contents("https://api.telegram.org/bot263336681:AAF9QR9Y17jBSk5mlvcG9Vmko4o_5wY4YJw/sendChatAction?chat_id=$uid&action=typing");
+curl_get_contents("https://api.telegram.org/bot263336681:AAF9QR9Y17jBSk5mlvcG9Vmko4o_5wY4YJw/sendChatAction?chat_id=$uid&action=typing");
 $msg = urlencode($upd['message']['text']);
 $name = urlencode($upd['message']['from']['first_name']);
-$res = file_get_contents("http://api.brainshop.ai/get?bid=219&key=5laVJtJPqcXzHMRe&uid=$uid-$name&msg=$msg");
+$res = curl_get_contents("http://api.brainshop.ai/get?bid=219&key=5laVJtJPqcXzHMRe&uid=$uid-$name&msg=$msg");
 $rep = urlencode(json_decode($res, true)['cnt']);
-file_get_contents("https://api.telegram.org/bot263336681:AAF9QR9Y17jBSk5mlvcG9Vmko4o_5wY4YJw/sendMessage?chat_id=$uid&parse_mode=HTML&text=$rep");
+curl_get_contents("https://api.telegram.org/bot263336681:AAF9QR9Y17jBSk5mlvcG9Vmko4o_5wY4YJw/sendMessage?chat_id=$uid&parse_mode=HTML&text=$rep");
 function curl_get_contents($url){
   static $ch;
   if !$ch $ch = curl_init();
